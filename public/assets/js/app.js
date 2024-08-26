4130691b6521056202bad74bc80fd22edd9c0733
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2024-08-26 15:55:29 +0000

    asssets

---
+++ public/assets/js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  AOS.init({
+    duration: 3000,
+    easeing: 'ease-in-out',
+    mirror: true,
+    anchorPlacement: 'top-bottom'
+  });
+
+  //scrollCue
+  scrollCue.init();
+
+// Atropos
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    // rest of parameters
+});
+
+})
+
+
+/*  Button Go Top */
+const btnGoTop = document.querySelector('.btn-gotop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) { 
+    btnGoTop.style.display = 'block';
+  } else {
+    btnGoTop.style.display = 'none';
+  }
+});
+
+btnGoTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
