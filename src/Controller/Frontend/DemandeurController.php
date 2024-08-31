0029ebfc8b87fb141346b242cb79251a49c13b5c
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2024-08-31 18:38:12 +0000

    Gestion Demandeur
    Layout demandeur

--- src/Controller/Frontend/DemandeurController.php
+++ src/Controller/Frontend/DemandeurController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Frontend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,5 +16,11 @@ class DemandeurController extends AbstractController
     public function tbord(): Response
     {
         return $this->render('frontend/demandeur_tboard.html.twig');
+    }
+
+    #[Route('/profile', name: 'app_frontend_demandeur_profile', methods: ['GET', 'POST'])]
+    public function profile(Request $request)
+    {
+        return $this->render('frontend/demandeur_profile.html.twig');
     }
 }
